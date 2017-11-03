3ec27370b18366e8c6b5a7ca1c518881d5c6beb7
Evan Farrer <efarrer@hirevue.com>
2017-11-02 22:05:04 -0600

    Fill out the page

--- science-survey.go
+++ science-survey.go
@@ -7,10 +7,60 @@ import (
 
 const VERSION = "0.1"
 
+const PAGE = `
+<html>
+  <head>
+  	<!-- %s -->
+    <style>
+        .intro {
+            padding-top:10%%;
+            padding-left:30%%;
+            padding-right:30%%;
+            font-size:larger;
+        }
+        .button {
+          font-size: larger;
+          text-decoration: none;
+          background-color: #0095ff;
+          color: #333333;
+          padding: 2px 6px 2px 6px;
+          border-top: 1px solid #CCCCCC;
+          border-right: 1px solid #333333;
+          border-bottom: 1px solid #333333;
+          border-left: 1px solid #CCCCCC;
+          box-shadow: inset 0 1px 0 #66bfff;
+          border-radius: 10px;
+        }
+    </style>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td class="intro">
+          <center>
+            Hi my name is Eli Farrer, and thank you for taking this survey. It will take about 1
+            minute and it will really help me with my science fair project. Please do not discuss any part of the
+            survey with anyone else to keep the survey unbiased. If you can please ask others to
+            also take the survey, I would appreciate it. The more people I get the better my data will be.
+            </center>
+        </td>
+      </tr>
+      <tr>
+        <td style="padding-top:5%%">
+          <center>
+            <a href="#" class="button">Begin Survey</a>
+          </center>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
+`
+
 func init() {
 	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Thanks for being willing to help with my science project by taking this survey! (%s)", VERSION)
+	fmt.Fprintf(w, PAGE, VERSION)
 }
